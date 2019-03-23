ac03363ff50028addce60a4c7acd697fd46e27b0
estebancode <bevarela6@misena.edu.co>
2019-03-23 15:09:58 -0500

    configuration cors

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 // settings
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.set('port', process.env.PORT || 4000);
 app.set('json spaces',2);
 
